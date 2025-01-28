88765c88d594d3f47c404421c770cc16d41f2ba5
Tim Reed <44115355+TimLikesTacos@users.noreply.github.com>
2025-01-28 05:27:20 -0800

    support mutable and immutable for sync

--- src/lib.rs
+++ src/lib.rs
@@ -21,41 +21,60 @@ pub use crate::sync::*;
 mod sync {
 
     use std::cell::RefCell;
-    //use std::rc::{Rc, Weak as RcWeak};
-    use std::sync::{Arc, Weak};
+    use std::rc::{Rc, Weak};
 
-    // enum Mutability<E> {
-    //     Mutable(Rc<RefCell<dyn Observer<E>>>),
-    //     Immutable(Rc<dyn Observer<E>>),
-    // }
+    enum Mutability<E> {
+        Mutable(Weak<RefCell<dyn ObserverMut<E>>>),
+        Immutable(Weak<dyn Observer<E>>),
+    }
 
     pub trait Observable<E> {
-        fn register_observer(&mut self, event: Arc<RefCell<dyn Observer<E>>>);
-        // fn register_observer_mut(&mut self, event: &dyn Observer<E>);
+        fn register_observer(&mut self, event: Rc<dyn Observer<E>>);
+        fn register_observer_mut(&mut self, event: Rc<RefCell<dyn ObserverMut<E>>>);
         fn notify_observers(&mut self, event: &E);
     }
 
     pub trait Observer<E> {
-        fn notify(&mut self, event: &E);
+        fn notify(&self, event: &E);
+    }
+
+    pub trait ObserverMut<E> {
+        fn notify_mut(&mut self, event: &E);
     }
 
     pub struct Dispatcher<E> {
-        observers: Vec<Weak<RefCell<dyn Observer<E>>>>,
+        observers: Vec<Mutability<E>>,
     }
 
     impl<E> Observable<E> for Dispatcher<E> {
-        fn register_observer(&mut self, observer: Arc<RefCell<dyn Observer<E>>>) {
-            self.observers.push(Arc::downgrade(&observer));
+        fn register_observer(&mut self, observer: Rc<dyn Observer<E>>) {
+            self.observers
+                .push(Mutability::Immutable(Rc::downgrade(&observer)));
+        }
+
+        fn register_observer_mut(&mut self, observer: Rc<RefCell<dyn ObserverMut<E>>>) {
+            self.observers
+                .push(Mutability::Mutable(Rc::downgrade(&observer)));
         }
 
         fn notify_observers(&mut self, event: &E) {
             // This removes any observers that have been dropped (fail to upgrade)
-            self.observers.retain(|observer| {
-                if let Some(observer) = observer.upgrade() {
-                    observer.borrow_mut().notify(event);
-                    true
-                } else {
-                    false
+            self.observers.retain(|observer| match observer {
+                Mutability::Mutable(observer) => {
+                    if let Some(observer) = observer.upgrade() {
+                        observer.borrow_mut().notify_mut(event);
+                        return true;
+                    } else {
+                        false
+                    }
+                }
+                Mutability::Immutable(observer) => {
+                    if let Some(observer) = observer.upgrade() {
+                        observer.notify(event);
+                        return true;
+                    } else {
+                        false
+                    }
                 }
             });
         }
@@ -77,92 +96,197 @@ mod sync {
     mod tests {
         use super::*;
 
-        struct Observer1(i32);
-        impl Observer<i32> for Observer1 {
-            fn notify(&mut self, event: &i32) {
-                self.0 += event;
-                println!("Observer1: {}, result {}", event, self.0);
+        mod immutable {
+            use super::*;
+            struct Observer1(i32);
+            impl Observer<i32> for Observer1 {
+                fn notify(&self, event: &i32) {
+                    println!("Observer1: {}, result {}", event, self.0);
+                }
+            }
+
+            struct Observer2(i32);
+            impl Observer<i32> for Observer2 {
+                fn notify(&self, event: &i32) {
+                    println!("Observer2: {}, result {}", event, self.0);
+                }
+            }
+
+            #[test]
+            fn test_dispatcher() {
+                let mut dispatcher = Dispatcher::new();
+                let observer1 = Rc::new(Observer1(0));
+                let observer2 = Rc::new(Observer2(1));
+
+                dispatcher.register_observer(observer1.clone());
+                dispatcher.register_observer(observer2.clone());
+
+                assert_eq!(dispatcher.num_oberservers(), 2);
+
+                dispatcher.notify_observers(&1);
+
+                dispatcher.notify_observers(&2);
+
+                assert_eq!(dispatcher.num_oberservers(), 2);
+            }
+
+            #[test]
+            fn scoped_observer() {
+                let mut dispatcher = Dispatcher::new();
+                let observer1 = Rc::new(Observer1(0));
+                let observer2 = Rc::new(Observer2(1));
+
+                dispatcher.register_observer(observer1.clone());
+                dispatcher.register_observer(observer2.clone());
+
+                assert_eq!(dispatcher.num_oberservers(), 2);
+
+                dispatcher.notify_observers(&1);
+                assert_eq!(dispatcher.num_oberservers(), 2);
+
+                // Drop showing that the dispatcher does not own the observers
+                drop(observer1);
+
+                assert_eq!(dispatcher.num_oberservers(), 2);
+
+                dispatcher.notify_observers(&2);
+
+                assert_eq!(dispatcher.num_oberservers(), 1);
+            }
+        }
+        mod mutable {
+            use super::*;
+            struct Observer1(i32);
+            impl ObserverMut<i32> for Observer1 {
+                fn notify_mut(&mut self, event: &i32) {
+                    self.0 += event;
+                    println!("Observer1: {}, result {}", event, self.0);
+                }
+            }
+
+            struct Observer2(i32);
+            impl ObserverMut<i32> for Observer2 {
+                fn notify_mut(&mut self, event: &i32) {
+                    self.0 *= event;
+                    println!("Observer2: {}, result {}", event, self.0);
+                }
+            }
+
+            #[test]
+            fn test_dispatcher() {
+                let mut dispatcher = Dispatcher::new();
+                let observer1 = Rc::new(RefCell::new(Observer1(0)));
+                let observer2 = Rc::new(RefCell::new(Observer2(1)));
+
+                dispatcher.register_observer_mut(observer1.clone());
+                dispatcher.register_observer_mut(observer2.clone());
+
+                assert_eq!(dispatcher.num_oberservers(), 2);
+
+                dispatcher.notify_observers(&1);
+
+                assert_eq!(observer1.borrow().0, 1);
+                assert_eq!(observer2.borrow().0, 1);
+
+                dispatcher.notify_observers(&2);
+
+                assert_eq!(dispatcher.num_oberservers(), 2);
+
+                assert_eq!(observer1.borrow().0, 3);
+                assert_eq!(observer2.borrow().0, 2);
+            }
+
+            #[test]
+            fn scoped_observer() {
+                let mut dispatcher = Dispatcher::new();
+                let observer1 = Rc::new(RefCell::new(Observer1(0)));
+                let observer2 = Rc::new(RefCell::new(Observer2(3)));
+
+                dispatcher.register_observer_mut(observer1.clone());
+                dispatcher.register_observer_mut(observer2.clone());
+
+                assert_eq!(dispatcher.num_oberservers(), 2);
+
+                dispatcher.notify_observers(&2);
+
+                assert_eq!(observer1.borrow().0, 2);
+                assert_eq!(observer2.borrow().0, 6);
+
+                // Drop showing that the dispatcher does not own the observers
+                drop(observer1);
+
+                assert_eq!(dispatcher.num_oberservers(), 2);
+
+                dispatcher.notify_observers(&2);
+
+                assert_eq!(dispatcher.num_oberservers(), 1);
+
+                assert_eq!(observer2.borrow().0, 12);
+            }
+
+            #[test]
+            fn scoped_dispatcher_ownership_demo() {
+                let mut dispatcher = Dispatcher::new();
+                let observer1 = Rc::new(RefCell::new(Observer1(0)));
+                let observer2 = Rc::new(RefCell::new(Observer2(3)));
+
+                dispatcher.register_observer_mut(observer1.clone());
+                dispatcher.register_observer_mut(observer2.clone());
+
+                assert_eq!(dispatcher.num_oberservers(), 2);
+
+                dispatcher.notify_observers(&2);
+
+                // Dropping dispatcher to show that ownership of the observers is not tied to the dispatcher
+                drop(dispatcher);
+
+                assert_eq!(observer1.borrow().0, 2);
+                assert_eq!(observer2.borrow().0, 6);
             }
         }
 
-        struct Observer2(i32);
-        impl Observer<i32> for Observer2 {
-            fn notify(&mut self, event: &i32) {
-                self.0 *= event;
-                println!("Observer2: {}, result {}", event, self.0);
+        mod mutable_and_immutable {
+            use super::*;
+            struct MutOb(i32);
+            impl ObserverMut<i32> for MutOb {
+                fn notify_mut(&mut self, event: &i32) {
+                    self.0 += event;
+                    println!("MutableOb: {}, result {}", event, self.0);
+                }
             }
-        }
 
-        #[test]
-        fn test_dispatcher() {
-            let mut dispatcher = Dispatcher::new();
-            let observer1 = Arc::new(RefCell::new(Observer1(0)));
-            let observer2 = Arc::new(RefCell::new(Observer2(1)));
+            struct ImmutOb(i32);
+            impl Observer<i32> for ImmutOb {
+                fn notify(&self, event: &i32) {
+                    println!("ImmutableOb: {}, result {}", event, self.0);
+                }
+            }
+            #[test]
+            fn test_dispatcher() {
+                let mut dispatcher = Dispatcher::new();
+                let mut_observer = Rc::new(RefCell::new(MutOb(0)));
+                let immut_observer = Rc::new(ImmutOb(1));
 
-            dispatcher.register_observer(observer1.clone());
-            dispatcher.register_observer(observer2.clone());
+                dispatcher.register_observer_mut(mut_observer.clone());
+                dispatcher.register_observer(immut_observer.clone());
 
-            assert_eq!(dispatcher.num_oberservers(), 2);
+                assert_eq!(dispatcher.num_oberservers(), 2);
 
-            dispatcher.notify_observers(&1);
+                dispatcher.notify_observers(&1);
 
-            assert_eq!(observer1.borrow().0, 1);
-            assert_eq!(observer2.borrow().0, 1);
+                assert_eq!(mut_observer.borrow().0, 1);
 
-            dispatcher.notify_observers(&2);
+                dispatcher.notify_observers(&2);
 
-            assert_eq!(dispatcher.num_oberservers(), 2);
+                assert_eq!(dispatcher.num_oberservers(), 2);
 
-            assert_eq!(observer1.borrow().0, 3);
-            assert_eq!(observer2.borrow().0, 2);
-        }
+                assert_eq!(mut_observer.borrow().0, 3);
 
-        #[test]
-        fn scoped_observer() {
-            let mut dispatcher = Dispatcher::new();
-            let observer1 = Arc::new(RefCell::new(Observer1(0)));
-            let observer2 = Arc::new(RefCell::new(Observer2(3)));
-
-            dispatcher.register_observer(observer1.clone());
-            dispatcher.register_observer(observer2.clone());
-
-            assert_eq!(dispatcher.num_oberservers(), 2);
-
-            dispatcher.notify_observers(&2);
-
-            assert_eq!(observer1.borrow().0, 2);
-            assert_eq!(observer2.borrow().0, 6);
-
-            // Drop showing that the dispatcher does not own the observers
-            drop(observer1);
-
-            assert_eq!(dispatcher.num_oberservers(), 2);
-
-            dispatcher.notify_observers(&2);
-
-            assert_eq!(dispatcher.num_oberservers(), 1);
-
-            assert_eq!(observer2.borrow().0, 12);
-        }
-
-        #[test]
-        fn scoped_dispatcher_ownership_demo() {
-            let mut dispatcher = Dispatcher::new();
-            let observer1 = Arc::new(RefCell::new(Observer1(0)));
-            let observer2 = Arc::new(RefCell::new(Observer2(3)));
-
-            dispatcher.register_observer(observer1.clone());
-            dispatcher.register_observer(observer2.clone());
-
-            assert_eq!(dispatcher.num_oberservers(), 2);
-
-            dispatcher.notify_observers(&2);
-
-            // Dropping dispatcher to show that ownership of the observers is not tied to the dispatcher
-            drop(dispatcher);
-
-            assert_eq!(observer1.borrow().0, 2);
-            assert_eq!(observer2.borrow().0, 6);
+                drop(immut_observer);
+                dispatcher.notify_observers(&2);
+                assert_eq!(mut_observer.borrow().0, 5);
+                assert_eq!(dispatcher.num_oberservers(), 1);
+            }
         }
     }
 }
